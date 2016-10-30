f108f8679631e91bf0f632b8d3a8d6689ccc104d
ivan <tanzwud.work@gmail.com>
2016-10-30 10:02:50 +0000

    buble sort manipulation

---
+++ src/python/_30days/bublesort.py
@@ -0,0 +1,14 @@
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+for index, val in a:
+    for z, vv in a:
+        if val > vv:
+            pass
+        else:
+            pass
+        pass
+
+print(a)
+
+
