558e6c7b575d830970fbdc5797118691173f148a
Ivan Katliarchuk <tanzwud.work@gmail.com>
2016-11-01 20:55:53 +0000

    remove resources

---
+++ src/python/datacamp/dstoolbox1/builtins.py
@@ -0,0 +1,19 @@
+'''
+
+define a function
+'''
+
+
+def square(param):
+    '''Return the square of a value.'''
+    new_value = param ** 2
+    return new_value
+
+
+num = (3)
+print(num)
+
+object1 = "data" + "analysis" + 'visualization'
+print(object1)
+
+print(type(object1))
